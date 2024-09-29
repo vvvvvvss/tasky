ce0f5d9000a78557a41d18e5bef6f5bba9b2c6c1
Varsha Shubhashri M <148562671+vvvvvvss@users.noreply.github.com>
2024-09-29 22:53:43 +0530

    Create tasky.js

---
+++ tasky.js
@@ -0,0 +1,35 @@
+const inputBox = document.getElementById("input");
+const list = document.getElementById("list");
+
+function AddTask(){
+    if(inputBox.value === ''){
+        alert("you must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+}
+
+list.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData(){
+    localStorage.setItem("data,list.innerHTML");
+}
+function showTask(){
+    list.innerHTML = localStorage.getItem("data")
+}
+showTask();
